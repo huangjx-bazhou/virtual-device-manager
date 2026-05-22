7ab56c46b8c6f04fffb3d743e40ae8a975cd0856
黄佳兴 <519207704@qq.com>
2026-05-22 10:57:59 +0800

    主窗口

---
+++ MainWindow.cpp
@@ -0,0 +1,33 @@
+﻿#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+class MainWindowPrivate
+{
+public:
+    MainWindowPrivate();
+
+    QScopedPointer<Ui::MainWindow> ui;
+};
+
+MainWindowPrivate::MainWindowPrivate() :
+    ui(new Ui::MainWindow)
+{
+}
+
+MainWindow::MainWindow(QWidget *parent, Qt::WindowFlags flags) :
+    QMainWindow(parent, flags),
+    d(new MainWindowPrivate)
+{
+    d->ui->setupUi(this);
+
+    d->ui->listWidget->setSelectionMode(QAbstractItemView::NoSelection);
+    d->ui->listWidget->setSpacing(8);
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::on_addBtn_clicked()
+{
+}
---
+++ MainWindow.h
@@ -0,0 +1,28 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_USE_NAMESPACE
+
+class MainWindowPrivate;
+
+/**
+ * @brief 主窗口
+ */
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = Qt::WindowFlags());
+    virtual ~MainWindow();
+
+private Q_SLOTS:
+    void on_addBtn_clicked();
+
+private:
+    Q_DISABLE_COPY(MainWindow)
+    QScopedPointer<MainWindowPrivate> d;
+};
+
+#endif // MAINWINDOW_H
